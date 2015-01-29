d16efe6697edf5341e7a09de7eef5c16cb2498ba
poniatna <poniatna@users.noreply.github.com>
2015-01-28 22:09:27 -0500

    some additional correction for second assigmen

--- exercise2.rb
+++ exercise2.rb
@@ -1,10 +1,11 @@
+# first problem:
+
 	a = 55.00
-	b = a / 100 * 30
-	d = a + b
+	b = a / 100 * 20
 	
-puts "The good tip on a 55 dollar meal is #{b} dollars,\n so one would need to pay #{d} dollars in total."
+puts "The good tip on a 55 dollar meal is #{b} dollars,\n so one would need to pay #{a + b} dollars in total."
 
-
+# second problem:
 
 	a = 45628
 	b = 7839
@@ -12,6 +13,8 @@ puts "The good tip on a 55 dollar meal is #{b} dollars,\n so one would need to p
 	
 puts "The result of multiplying #{a} times #{b} eaquals #{c}."
 puts "Those above things have nothing in common, but use of numbers."
+
+#third problem:
 
 (true && false) #would return "false" statment
 (false && true) #would return "false" statment
