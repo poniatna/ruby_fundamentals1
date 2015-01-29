8b965d0641bc61d7aabfc49000332015feab5314
poniatna <poniatna@users.noreply.github.com>
2015-01-28 20:31:34 -0500

    Exercise 2 completed

---
+++ exercise2.rb
@@ -0,0 +1,17 @@
+	a = 55
+	b = 20
+	c = a % b 
+puts "The good tip on a 55 dollar meal is #{a % b} dollars,\n so one would need to pay #{ a + c} dollars in total."
+
+
+
+	a = 45628
+	b = 7839
+	c = a * b
+	
+puts "The result of multiplying #{a} times #{b} eaquals #{c}."
+puts "Those above things have nothing in common, but use of numbers."
+
+(true && false) #would return "false" statment
+(false && true) #would return "false" statment
+!(false && false) #would return "true" statment
---
+++ exercise_1a.rb
@@ -0,0 +1,9 @@
+	#puts "Enter name"
+	#name = gets.chomp
+	#puts "Your name is #{name}"
+
+a = 20
+b = 65
+puts "The value of a is #{a}."
+puts "The value of b is #{b}."
+puts "The value of a plus b is #{a + b}."
