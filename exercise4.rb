93c31ab9b3a0fb98742c2a2ac6d49491ff464c81
poniatna <poniatna@users.noreply.github.com>
2015-01-29 07:57:53 -0500

    Comments added

--- exercise4.rb
+++ exercise4.rb
@@ -1,4 +1,9 @@
-
+#Perhaps this is not the most beautiful code in the world, but it works.
+#For unknown to humanity reason, and by "humanity" I mean me,
+# "elsif" statmentent did not work and for that and couple more reasons is lenghty, repetetive, etc
+#However I am strong beliver in working code having an enormous adventage 
+#over even the most beautiful, but not working code.
+#
 
 
 one_to_hundred = (0..99)
